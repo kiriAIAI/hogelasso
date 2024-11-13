f736d9b6b85602cf846d9f728830fb21c62b050e
kiriAIAI <footblolg6@gmail.com>
2024-11-14 03:10:59 +0900

    test

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, send_from_directory, jsonify, request, redirect, url_for
-import flask
+import os
 import mysql.connector
 
 app = Flask(__name__, template_folder='kakikko')
