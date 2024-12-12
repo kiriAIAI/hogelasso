c60f6fd1156bd208b05811693c821d721abe4bba
kiriAIAI <footblolg6@gmail.com>
2024-12-12 13:43:58 +0900

    profile

--- database_dump.sql
+++ database_dump.sql
@@ -7,7 +7,7 @@
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES gbk */;
+/*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -39,7 +39,7 @@ CREATE TABLE `books` (
 
 LOCK TABLES `books` WRITE;
 /*!40000 ALTER TABLE `books` DISABLE KEYS */;
-INSERT INTO `books` VALUES (1,'Sample Book','This is a test book.','Test Category','10.00','test_cover.jpg',1),(2,'Python���T','Python�ץ�����ߥ󥰤���A����ѧ�٤�������򤱤Υ����ɤǤ���','�ץ�����ߥ�','2200.00','/images/python_intro.jpg',20000),(3,'MySQL��ȫ����','MySQL�Υǩ`���٩`������������⤹�뤿��Όg���Ĥʕ����Ǥ���','�ǩ`���٩`��','3500.00','/images/mysql_mastery.jpg',20000),(4,'�������_�k�λ��A','HTML��CSS��JavaScript�λ�����ѧ�٤�������򤱤�һ�ԤǤ���','�������_�k','2800.00','/images/web_basics.jpg',20000),(5,'AI�șCеѧ���λ��A','�˹�֪�ܤșCеѧ���λ����Ĥ��˽M�ߤ�狼��䤹�����h���ޤ���','�˹�֪��','4500.00','/images/ai_ml.jpg',20000),(6,'�����Щ`�������ƥ��λ��A','�ͥåȥ�`������󥻥����ƥ�����Ҫ�ʸ����W�_����һ�ԤǤ���','�������ƥ�','3200.00','/images/cybersecurity.jpg',20000),(7,'������','����','lifestyle','280000.00','GGG.jpg',20000),(8,'ֱ�ä�����','aa','lifestyle','0.00','G (3).jpg',20000),(9,'aaa','aaaa','lifestyle','0.00','unnamed.jpg',20000),(10,'aa','aa','lifestyle','280000.00','unnamed.jpg',20000),(11,'a','aaa','certification','280000.00','G (3).jpg',20000),(12,'aa','aa','lifestyle','280000.00','GGG.jpg',20000),(13,'a','aaa','lifestyle','0.00','GGG.jpg',20000),(14,'aa','aa','social','280000.00','��������.png',20000),(15,'a','aaa','history','280000.00','file',20000),(16,'a','aa','lifestyle','0.00','GGG.jpg',20000),(17,'a','aa','social','0.00','file',20000),(18,'a','aa','history','280000.00','GGG.jpg',20000),(19,'��������','������','lifestyle','222.00','G (3).jpg',20000),(20,'�ԥۥ�Ȥˤ��꤬�Ȥ�','aaa','lifestyle','0.00','GGG.jpg',100000),(21,'���դ���դȤ�','aa','certification','280000.00','������`�󥷥�å� 2023-03-19 004028.png',100000),(22,'�������ȡ��ʤ���Фʤ�ʤ�','����','lifestyle','303000.00','������`�󥷥�å� 2024-04-26 224626.png',100000),(23,'kakikko','������','lifestyle','280000.00','������`�󥷥�å� 2024-11-13 190333.png',100000),(24,'test','<div style=\"text-align: right;\"><i style=\"font-size: 1.7rem; color: var(--dark-gray);\">test</i></div>','literature','3000.00','133571682160460525.jpg',1),(25,'test2','test2','history','2000.00','133590530057476704.jpg',1);
+INSERT INTO `books` VALUES (1,'Sample Book','This is a test book.','Test Category','10.00','test_cover.jpg',1),(2,'Python锟斤拷锟絋','Python锟阶ワ拷锟斤拷锟斤拷撺螗挨锟斤拷锟紸锟斤拷锟斤拷学锟劫わ拷锟斤拷锟斤拷锟斤拷颏堡违锟斤拷锟斤拷嗓扦锟斤拷锟','锟阶ワ拷锟斤拷锟斤拷撺锟','2200.00','/images/python_intro.jpg',20000),(3,'MySQL锟斤拷全锟斤拷锟斤拷','MySQL锟轿デー锟斤拷锟劫ー锟斤拷锟斤拷锟斤拷锟斤拷锟筋く锟斤拷锟解す锟诫た锟斤拷螌g锟斤拷锟侥な曪拷锟斤拷锟角わ拷锟斤拷','锟角ー锟斤拷锟劫ー锟斤拷','3500.00','/images/mysql_mastery.jpg',20000),(4,'锟斤拷锟斤拷锟斤拷锟絖锟絢锟轿伙拷锟紸','HTML锟斤拷CSS锟斤拷JavaScript锟轿伙拷锟斤拷锟斤拷学锟劫わ拷锟斤拷锟斤拷锟斤拷颏堡锟揭伙拷预扦锟斤拷锟','锟斤拷锟斤拷锟斤拷锟絖锟絢','2800.00','/images/web_basics.jpg',20000),(5,'AI锟饺機械学锟斤拷锟轿伙拷锟紸','锟剿癸拷知锟杰と機械学锟斤拷锟轿伙拷锟斤拷锟侥わ拷锟剿組锟竭わ拷铯?拷锟戒す锟斤拷锟斤拷锟絟锟斤拷锟睫わ拷锟斤拷','锟剿癸拷知锟斤拷','4500.00','/images/ai_ml.jpg',20000),(6,'锟斤拷锟斤拷锟叫ー锟斤拷锟斤拷锟斤拷锟狡ワ拷锟轿伙拷锟紸','锟酵ットワ拷`锟斤拷锟斤拷锟斤拷螗互锟斤拷锟斤拷匹锟斤拷锟斤拷锟揭?拷矢锟斤拷锟斤拷W锟絖锟斤拷锟斤拷一锟皆でわ拷锟斤拷','锟斤拷锟斤拷锟斤拷锟狡ワ拷','3200.00','/images/cybersecurity.jpg',20000),(7,'锟斤拷锟斤拷锟斤拷','锟斤拷锟斤拷','lifestyle','280000.00','GGG.jpg',20000),(8,'直锟矫わ拷锟斤拷锟斤拷','aa','lifestyle','0.00','G (3).jpg',20000),(9,'aaa','aaaa','lifestyle','0.00','unnamed.jpg',20000),(10,'aa','aa','lifestyle','280000.00','unnamed.jpg',20000),(11,'a','aaa','certification','280000.00','G (3).jpg',20000),(12,'aa','aa','lifestyle','280000.00','GGG.jpg',20000),(13,'a','aaa','lifestyle','0.00','GGG.jpg',20000),(14,'aa','aa','social','280000.00','锟斤拷锟斤拷锟斤拷锟斤拷.png',20000),(15,'a','aaa','history','280000.00','file',20000),(16,'a','aa','lifestyle','0.00','GGG.jpg',20000),(17,'a','aa','social','0.00','file',20000),(18,'a','aa','history','280000.00','GGG.jpg',20000),(19,'锟斤拷锟斤拷锟斤拷锟斤拷','锟斤拷锟斤拷锟斤拷','lifestyle','222.00','G (3).jpg',20000),(20,'锟皆ホワ拷趣摔锟斤拷辘?拷趣锟','aaa','lifestyle','0.00','GGG.jpg',100000),(21,'锟斤拷锟秸わ拷锟斤拷栅趣锟','aa','certification','280000.00','锟斤拷锟斤拷锟斤拷`锟襟シワ拷氓锟?2023-03-19 004028.png',100000),(22,'锟斤拷锟斤拷锟斤拷锟饺★拷锟绞わ拷锟斤拷肖胜锟绞わ拷','锟斤拷锟斤拷','lifestyle','303000.00','锟斤拷锟斤拷锟斤拷`锟襟シワ拷氓锟?2024-04-26 224626.png',100000),(23,'kakikko','锟斤拷锟斤拷锟斤拷','lifestyle','280000.00','锟斤拷锟斤拷锟斤拷`锟襟シワ拷氓锟?2024-11-13 190333.png',100000),(24,'test','<div style=\"text-align: right;\"><i style=\"font-size: 1.7rem; color: var(--dark-gray);\">test</i></div>','literature','3000.00','133571682160460525.jpg',1),(25,'test2','test2','history','2000.00','133590530057476704.jpg',1);
 /*!40000 ALTER TABLE `books` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -249,7 +249,7 @@ CREATE TABLE `user_profiles` (
 
 LOCK TABLES `user_profiles` WRITE;
 /*!40000 ALTER TABLE `user_profiles` DISABLE KEYS */;
-INSERT INTO `user_profiles` VALUES (6,'久保出','泰介',NULL,NULL,NULL,NULL,'aaa','user_6_unnamed.jpg'),(20000,'','',NULL,NULL,NULL,NULL,'',NULL);
+INSERT INTO `user_profiles` VALUES (6,'タ','イスケ',NULL,NULL,NULL,NULL,'aa','user_6_GGGx.jpg'),(20000,'久保出','泰介',NULL,NULL,NULL,NULL,'aa','user_20000_png'),(100000,'タ','イスケ',NULL,NULL,NULL,NULL,'aaa','user_100000_png');
 /*!40000 ALTER TABLE `user_profiles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -295,7 +295,7 @@ CREATE TABLE `user_security_questions` (
 
 LOCK TABLES `user_security_questions` WRITE;
 /*!40000 ALTER TABLE `user_security_questions` DISABLE KEYS */;
-INSERT INTO `user_security_questions` VALUES (1,'СѧУ�Εr�Τ�����','ƽƽ','�����Ή�','��֤��ʤꤿ��'),(2,'СѧУ�Εr�Τ�����','111','��У�Εr�Τ�����','111'),(3,'СѧУ�Εr�Τ�����','123','��У�Εr�Τ�����','123'),(4,'СѧУ�Εr�Τ�����','123','��У�Εr�Τ�����','123'),(5,'СѧУ�Εr�Τ�����','123','��У�Εr�Τ�����','123'),(6,'СѧУ�Εr�Τ�����','test1','�����Ή�','test1'),(100000,'СѧУ�Εr�Τ�����','�ܤ�','��У�Εr�Τ�����','�ܤ�'),(100001,'СѧУ�Εr�Τ�����','haha','��У�Εr�Τ�����','haha');
+INSERT INTO `user_security_questions` VALUES (1,'小学校锟轿時锟轿わ拷锟斤拷锟斤拷','平平','锟斤拷锟斤拷锟轿夛拷','锟斤拷证锟斤拷胜辘匡拷锟'),(2,'小学校锟轿時锟轿わ拷锟斤拷锟斤拷','111','锟斤拷校锟轿時锟轿わ拷锟斤拷锟斤拷','111'),(3,'小学校锟轿時锟轿わ拷锟斤拷锟斤拷','123','锟斤拷校锟轿時锟轿わ拷锟斤拷锟斤拷','123'),(4,'小学校锟轿時锟轿わ拷锟斤拷锟斤拷','123','锟斤拷校锟轿時锟轿わ拷锟斤拷锟斤拷','123'),(5,'小学校锟轿時锟轿わ拷锟斤拷锟斤拷','123','锟斤拷校锟轿時锟轿わ拷锟斤拷锟斤拷','123'),(6,'小学校锟轿時锟轿わ拷锟斤拷锟斤拷','test1','锟斤拷锟斤拷锟轿夛拷','test1'),(100000,'小学校锟轿時锟轿わ拷锟斤拷锟斤拷','锟杰わ拷','锟斤拷校锟轿時锟轿わ拷锟斤拷锟斤拷','锟杰わ拷'),(100001,'小学校锟轿時锟轿わ拷锟斤拷锟斤拷','haha','锟斤拷校锟轿時锟轿わ拷锟斤拷锟斤拷','haha');
 /*!40000 ALTER TABLE `user_security_questions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -318,7 +318,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'��ƽ','xiangping@gmail.com','xiangping'),(2,'trump','president@gmail.com','maga'),(3,'123','123@gmail.com','123'),(4,'test','9196723@ha.com','test'),(5,'222','222@gmail.com','222'),(6,'test1','test1@gmail.com','test1'),(20000,'BuyerUser','buyer@example.com','buyerpassword'),(99999,'kazuto','kazuto@gmail.com','kazuto0330'),(100000,'bode','bode@gmail.com','bode'),(100001,'haha','haha@qq.com','haha');
+INSERT INTO `users` VALUES (1,'锟斤拷平','xiangping@gmail.com','xiangping'),(2,'trump','president@gmail.com','maga'),(3,'123','123@gmail.com','123'),(4,'test','9196723@ha.com','test'),(5,'222','222@gmail.com','222'),(6,'test1','test1@gmail.com','test1'),(20000,'BuyerUser','buyer@example.com','buyerpassword'),(99999,'kazuto','kazuto@gmail.com','kazuto0330'),(100000,'bode','bode@gmail.com','bode'),(100001,'haha','haha@qq.com','haha');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -331,4 +331,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-12-12  4:03:21
+-- Dump completed on 2024-12-12  4:43:16
