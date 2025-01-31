6b1ce97328f54714636f75e85621139121306d87
kiriAIAI <footblolg6@gmail.com>
2025-01-31 23:59:15 +0900

    connection no

--- app.py
+++ app.py
@@ -1629,4 +1629,4 @@ app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SESSION_TYPE'] = 'filesystem'
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=80) #IPアドレスとポート番号はマシンに合わせて変更
+    app.run(debug=False)
